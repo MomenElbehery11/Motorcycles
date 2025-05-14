a96ec0ae12d6d7a49fd19d933d298df6c0425e5e
MomenElbehery11 <momen_elbehery11@yahoo.com>
2025-05-14 23:00:48 +0300

    Initial commit after reinitializing

--- app/Http/Controllers/api/ImageController.php
+++ app/Http/Controllers/api/ImageController.php
@@ -8,27 +8,8 @@ use App\Models\Image;
 
 class ImageController extends Controller
 {
-    public function index()
+   public function index()
     {
-        $images = Image::all();
-        return response()->json($images);
-    }
-
-    public function create()
-    {
-        // You might want to return a JSON response or some data
-        return response()->json(['message' => 'Create image']);
-    }
-
-    public function store(Request $request)
-    {
-        // Handle image store
-        $image = new Image();
-        $image->path = $request->input('path');
-        // other attributes...
-
-        $image->save();
-
-        return response()->json(['message' => 'Image uploaded successfully', 'image' => $image], 201);
+        return response()->json(Image::all());
     }
 }
